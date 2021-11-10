f52552ce355d3798b1b7490183cc40bef1761912
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2021-11-10 09:41:32 +0000

    Formatted with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import { greet } from "./utils/greet";
 // import episodes from "./episodes.json";
-import EpisodesView from "./components/Episode"
-
+import EpisodesView from "./components/Episode";
 
 function App(): JSX.Element {
-  return <>
-    {/* <h1>{greet("World")}</h1> */}
-    <EpisodesView />
-  </>
+  return (
+    <>
+      {/* <h1>{greet("World")}</h1> */}
+      <EpisodesView />
+    </>
+  );
 }
 
 export default App;
--- src/components/Episode.tsx
+++ src/components/Episode.tsx
@@ -1,30 +1,39 @@
 import episodes from "../episodes.json";
 import IEpisode from "../utils/interface";
 
-type OneEpisodeProps = { episode: IEpisode }
+type OneEpisodeProps = { episode: IEpisode };
 
 export default function EpisodesView(): JSX.Element {
-    const episodesList = episodes.map((episode)=> <Episode key={episode.id} episode={episode}/>)
-    
-    return <>
-    <div>{episodesList}</div>
+  const episodesList = episodes.map((episode) => (
+    <Episode key={episode.id} episode={episode} />
+  ));
+
+  return (
+    <>
+      <div>{episodesList}</div>
     </>
+  );
 }
 
 function Episode({ episode }: OneEpisodeProps): JSX.Element {
-    return <>
-    <h2>S{padSeasonOrNumber(episode.season)}E{padSeasonOrNumber(episode.number)} {episode.name}</h2>
-    <img src={episode.image.medium} alt="Scene from episode" />
-    <p>{trimSummary(episode.summary)}</p>
-    <a href={episode.url}>Source</a>
+  return (
+    <>
+      <h2>
+        S{padSeasonOrNumber(episode.season)}E{padSeasonOrNumber(episode.number)}{" "}
+        {episode.name}
+      </h2>
+      <img src={episode.image.medium} alt="Scene from episode" />
+      <p>{trimSummary(episode.summary)}</p>
+      <a href={episode.url}>Source</a>
     </>
+  );
 }
 
 function padSeasonOrNumber(seasonOrEpNum: number): string {
-    return seasonOrEpNum.toString().padStart(2, "0")
+  return seasonOrEpNum.toString().padStart(2, "0");
 }
 
 function trimSummary(summary: string): string {
-    const summaryLength = summary.length;
-    return summary.slice(3, summaryLength-4)
+  const summaryLength = summary.length;
+  return summary.slice(3, summaryLength - 4);
 }
